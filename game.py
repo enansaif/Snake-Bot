baaadb40f1531a102508d5020150dbceaffb74ff
rect1fy <enansaifme33@gmail.com>
2023-03-04 11:47:56 +0600

    Complete snake v1.0

--- game.py
+++ game.py
@@ -1,24 +1,21 @@
+from collections import namedtuple
 import random
 import pygame
-from collections import namedtuple
-
-colors = {'black' : (0, 0, 0),
-          'white' : (255,255,255),
-          'red' : (255,0,0),
-          'green' : (0,255,0),
-          'blue' : (0,0,255)}
-
-point = namedtuple('point', 'x, y')
-speed = 40
-block_size = 20
 pygame.init()
+
 font = pygame.font.SysFont('arial', 20)
+colors = {'black' : (0, 0, 0), 'white' : (255,255,255),
+          'red' : (255,0,0), 'green' : (0,255,0), 'blue' : (0,0,255)}
+point = namedtuple('point', 'x, y')
+block_size = 20
+
 class Game:
 
     def __init__(self, width=1280, height=720):
         self.w = width
         self.h = height
         self.score = 0
+        self.speed = 8
         # main game display
         self.display = pygame.display.set_mode((self.w, self.h))
         pygame.display.set_caption('Snake')
@@ -29,7 +26,6 @@ class Game:
                       point(self.head.x - (2*block_size), self.head.y)]
         self.food = None
         self.place_food()
-        
 
     def place_food(self):
         #get position as multiple of block_size
@@ -38,28 +34,78 @@ class Game:
         self.food = point(x, y)
         while self.food in self.snake:
             self.place_food()
-
-    def play_step(self):
-        # collect user input 
-        # move
-        # check if game over
-        # place new food
-        # update ui/clock
-        self.update_ui()
-        self.clock.tick(speed)
-        # return game over/score
-        game_over = False
-        return game_over, self.score
     
+    def move(self, direction):
+        x = self.head.x
+        y = self.head.y
+        if direction == "right":
+            x += block_size
+        elif direction == "left":
+            x -= block_size
+        elif direction == "up":
+            y -= block_size
+        elif direction == "down":
+            y += block_size
+        self.head = point(x, y)
+
+    def collision(self):
+        # boundary collision
+        if (self.head.x > self.w - block_size or self.head.x < 0 or 
+            self.head.y > self.h - block_size or self.head.y < 0):
+            return True
+        # snake collision
+        if self.head in self.snake[1:]:
+            return True
+        return False
+
     def update_ui(self):
         self.display.fill(colors['black'])
         for p in self.snake:
-            pygame.draw.rect(self.display, colors['white'], pygame.Rect(p.x, p.y, block_size, block_size))
-        pygame.draw.rect(self.display, colors['green'], pygame.Rect(self.food.x, self.food.y, block_size, block_size))
+            pygame.draw.rect(self.display, colors['white'], 
+                             pygame.Rect(p.x, p.y, block_size, block_size))
+        pygame.draw.rect(self.display, colors['green'], 
+                         pygame.Rect(self.food.x, self.food.y, block_size, block_size))
         
         text = font.render(f"Score:{self.score}", True, colors['white'])
-        self.display.blit(text, [0, 0])
+        self.display.blit(text, (0, 0))
         pygame.display.flip()
+    
+    def play_step(self):
+        # collect user input 
+        for even in pygame.event.get():
+            if even.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if even.type == pygame.KEYDOWN:
+                if even.key == pygame.K_LEFT:
+                    self.direction = "left"
+                elif even.key == pygame.K_RIGHT:
+                    self.direction = "right"
+                elif even.key == pygame.K_UP:
+                    self.direction = "up"
+                elif even.key == pygame.K_DOWN:
+                    self.direction = "down"
+        # move
+        self.move(self.direction) #update head
+        self.snake.insert(0, self.head)
+        # check if game over
+        game_over = False
+        if self.collision():
+            game_over = True
+            return game_over, self.score
+        # place new food
+        if self.head == self.food:
+            self.score += 1
+            self.speed += 1
+            self.place_food()
+        else:
+            self.snake.pop()
+        # update ui/clock
+        self.update_ui()
+        self.clock.tick(self.speed)
+        # return game over/score
+        
+        return game_over, self.score
 
 if __name__ == '__main__':
     game = Game()
